116497bcf4f17d33c5473eb91856ce2586af5127
Irina <iratushina17@gmail.com>
2021-04-26 19:45:43 +0100

    Single post view added

---
+++ resources/views/post.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="Description" content="Enter your description here"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+<link rel="stylesheet" href="assets/css/app.css">
+<title>My Post</title>
+</head>
+<body>
+
+
+    <article>
+        <h1><a href="/post"> My First Post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quos, vel numquam aliquam magni sint praesentium atque. Facere, saepe reprehenderit. Natus facilis officia asperiores, incidunt architecto obcaecati quae reiciendis officiis.</p>
+    </article>
+    
+    <a href="/">Go back</a>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="Description" content="Enter your description here"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+<link rel="stylesheet" href="assets/css/app.css">
+<title>My Blog</title>
+</head>
+<body>
+
+
+    <article>
+        <h1><a href="/post"> My First Post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quos, vel numquam aliquam magni sint praesentium atque. Facere, saepe reprehenderit. Natus facilis officia asperiores, incidunt architecto obcaecati quae reiciendis officiis.</p>
+    </article>
+    <article>
+        <h1><a href="/post"> My SecondPost</a></h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quos, vel numquam aliquam magni sint praesentium atque. Facere, saepe reprehenderit. Natus facilis officia asperiores, incidunt architecto obcaecati quae reiciendis officiis.</p>
+    </article>
+    <article>
+        <h1><a href="/post"> My Third Post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quos, vel numquam aliquam magni sint praesentium atque. Facere, saepe reprehenderit. Natus facilis officia asperiores, incidunt architecto obcaecati quae reiciendis officiis.</p>
+    </article>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+</body>
+</html>
